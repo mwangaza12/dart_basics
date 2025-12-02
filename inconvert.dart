84daf7b2789bbbd489c20925aa0097bafde3d920
mwangaza12 <josephmwangaza1@gmail.com>
2025-12-02 15:34:07 +0300

    user input type conversion

---
+++ inconvert.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  //user input type conversion
+
+  print("Enter a number:");
+
+  var num = stdin.readLineSync();
+
+  var num2 = int.parse(num ?? '0') + 10;
+  print("$num + 10 = $num2");
+}
