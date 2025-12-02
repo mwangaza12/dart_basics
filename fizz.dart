91c69ec7891c9ce8e6d16d2a341401bcf31e64b0
mwangaza12 <josephmwangaza1@gmail.com>
2025-12-02 15:41:36 +0300

    fizzbuzz challeng

---
+++ fizz.dart
@@ -0,0 +1,16 @@
+void main() {
+  int num = 1;
+
+  while (num <= 100) {
+    if(num % 3 == 0 && num % 5 == 0) {
+      print("$num. FizzBuzz");
+    } else if (num % 3 == 0) {
+      print("$num. Fizz");
+    } else if (num % 5 == 0) {
+      print("$num. Buzz");
+    } else {
+      print(num);
+    }
+    num++;
+  }
+}
