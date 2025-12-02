841433741d428df26ed326a60a7df4551eadb0df
mwangaza12 <josephmwangaza1@gmail.com>
2025-12-02 15:25:03 +0300

    data type conversion

---
+++ convert.dart
@@ -0,0 +1,23 @@
+void main() {
+  // convert string integer double
+  // Strings to int
+  var a, b, c;
+  a = 40;
+  b = "1";
+  c = a + int.parse(b);
+  print("$a + $b = $c");
+
+  // Strings to double
+  var d, e, f;
+  d = 40;
+  e = "0.1";
+  f = d + double.parse(e);
+  print("$d + $e =  $f");
+
+  // int to String
+  var g, h, i;
+  g = 40;
+  h = "1";
+  i = g.toString() + h;
+  print("$g + $h = $i");
+}
